24d6d346d452244a79c6d0db122dc82ddc7e758b
groovehost <host@groove.cafe>
2020-09-17 18:31:41 -0400

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/red-logo-manifest.png`, // This path is relative to the root of the site.
       },
     },
     {
